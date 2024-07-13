25a730e2889a20f731688c9bc02b6820050ea252
artemdost <dartem925@gmail.com>
2024-07-13 15:18:07 +0300

    swap

---
+++ NewSwap/First.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./ERC20.sol";
+
+/**
+ * @author  .
+ * @title   .
+ * @dev     .
+ * @notice  .
+ */
+
+contract First is ERC20 {
+
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply) ERC20(name_, symbol_) {
+        owner = msg.sender;
+        Mint(initialSupply);
+    }
+
+
+    function Mint(uint _toMint) public isOwner(){
+        _totalSupply += _toMint;
+        _balances[owner] += _toMint;
+    } 
+}
---
+++ NewSwap/Swap.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./Second.sol";
+import "./First.sol";
+import "./IERC20.sol";
+
+
+contract Swap{
+    First first;
+    Second second;
+
+    constructor(){
+        first = First(0xA6D26090a198B63Dc29712370F2f66ACfffdaEF1);
+        second = Second(0xcABA4B9660824F1D4afC888D7995A023391C9e2f);
+    }
+
+    struct OwnsTokens{
+        uint first;
+        uint second;
+    }
+
+    mapping (address => OwnsTokens) userLiq; // тут хранится соответствующее количество токенов, которое прислано пользователем на данный контракт.
+
+    function swap(uint _tokenId, uint256 amount) public{
+        if (_tokenId == 1){
+            userLiq[msg.sender].first = first.balanceOf(address(this));
+            require(userLiq[msg.sender].first >= amount, "User did not send enought funds to swap");
+            second.transfer(msg.sender, amount);
+        } else if (_tokenId == 2){
+            userLiq[msg.sender].second = second.balanceOf(address(this));
+            require(userLiq[msg.sender].second >= amount, "User did not send enought funds to swap");
+            first.transfer(msg.sender, amount);
+        }
+    }
+
+    function currentLiq() public returns (uint, uint){
+        userLiq[msg.sender].first = first.balanceOf(address(this));
+        userLiq[msg.sender].second = second.balanceOf(address(this));
+        return (userLiq[msg.sender].first, userLiq[msg.sender].second);
+    }
+    
+}
